f334ab7f773ae10a235610c327271930f4115c22
Josh Rickard <josh.rickard@MacBook-Pro.local>
2020-10-19 11:48:56 -0500

    Udpating to work with new auth redirect

--- revivesocialmedia/browser.py
+++ revivesocialmedia/browser.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -19,4 +20,7 @@ class Browser:
         password.send_keys(self.password)
         sign_in = browser.find_element_by_xpath("//button[@type='submit']")
         sign_in.click()
+        time.sleep(5)
+        allow = browser.find_element_by_id("oauth__auth-form__submit-btn")
+        allow.click()
         return browser.current_url.split('?code=')[1].split('&state=')[0]
